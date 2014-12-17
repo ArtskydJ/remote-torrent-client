7375a762cf2c833c4d2b32b44281ee8d1726241d
Joseph Dykstra <josephdykstra@gmail.com>
2014-12-17 13:47:57 -0600

    0.0.1

--- client.js
+++ client.js
@@ -1,27 +1,29 @@
 var fs = require('fs')
 var path = require('path')
-var config = require('./config.json')
+var crypto = require('crypto')
 var SocketIoClient = require('socket.io-client')
 var ss = require('socket.io-stream')
-var crypto = require('crypto')
 
-var socket = new SocketIoClient(config.host)
 
 var trntStr = process.argv[2]
 var authStr = process.argv[3]
+var hostStr = process.argv[4] || "http://localhost:6880"
 if (!trntStr || !authStr || !/:/.test(authStr)) {
 	console.log(
 		'Usage:\n' +
-		'  node client.js [trnt] [auth]\n' +
+		'  node client.js trnt auth [host]\n' +
 		'trnt can be one of the following:\n' +
 		'  [magnet uri]\n' +
 		'  [info hash]\n' +
 		'  [http(s) path to torrent file]\n' +
 		'auth must be formatted like:\n' +
-		'  [username]:[password]'
+		'  [username]:[password]\n' +
+		'host must be formatted like:\n' +
+		'  [url]:[port]'
 	)
 	quit('\nIncorrect usage', 1)
 }
+var socket = new SocketIoClient(hostStr)
 
 socket.on('connect', function () {
 	var cred = authStr.split(':')
